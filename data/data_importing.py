25ee4694bcccc0828511ee35a88bb5321efec23e
DylanKalisvaart <d.kalisvaart@tudelft.nl>
2021-01-27 10:05:02 +0100

    Added csv 2 np to data_importing

--- data/data_importing.py
+++ data/data_importing.py
@@ -1,1 +1,13 @@
-#Placeholder module
+from numpy import genfromtxt
+
+def csv_2_np(datafile):
+    '''
+    data = csv_2_np(datafile) converts a csv file to a numpy array
+    
+    Inputs:
+        datafile: csv file
+        
+    Outputs:
+        data: numpy array containing data
+    '''
+    return genfromtxt(datafile, delimiter=',')
